31617a7cb469e7ed3e937258db83d2b044a78bb4
Nizar Zitouni <49596893+nizarzitouni@users.noreply.github.com>
2022-07-23 16:26:43 +0100

    Initial commit

---
+++ lib/button_changer.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+import 'widget/neumorphism_button_widget.dart';
+
+class ButtonChanger extends StatefulWidget {
+  const ButtonChanger({Key? key}) : super(key: key);
+
+  @override
+  State<ButtonChanger> createState() => _ButtonChangerState();
+}
+
+class _ButtonChangerState extends State<ButtonChanger> {
+  bool _darkMode = false;
+  bool _threeDOn = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _darkMode ? Colors.grey[850] : Colors.grey[300],
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NeumorphismButtonWidget(
+            darkMode: _darkMode,
+            height: 200,
+            width: 200,
+            borderRadiusOfButton: 50,
+            threeDOn: _threeDOn,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _darkMode = true;
+                    });
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.all(2)),
+                    elevation: MaterialStateProperty.all(0.0),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        side: BorderSide(color: Colors.white))),
+                    backgroundColor: MaterialStateProperty.all(Colors.black),
+                  ),
+                  child: Text(
+                    'Dark',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _darkMode = false;
+                    });
+                  },
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(EdgeInsets.all(2)),
+                    elevation: MaterialStateProperty.all(0.0),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                        side: BorderSide(color: Colors.white))),
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
+                  child: Text(
+                    'Light',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _threeDOn = !_threeDOn;
+                });
+              },
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                elevation: MaterialStateProperty.all(0.0),
+                shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    side: BorderSide(color: Colors.white))),
+                backgroundColor: MaterialStateProperty.all(
+                    _threeDOn ? Colors.red : Colors.blue),
+              ),
+              child: Text(
+                _threeDOn ? '3D OFF' : '3D ON',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/widget/neumorphism_button_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class NeumorphismButtonWidget extends StatelessWidget {
+  final bool darkMode;
+  final bool threeDOn;
+  final double width;
+  final double height;
+  final double borderRadiusOfButton;
+
+  const NeumorphismButtonWidget(
+      {super.key,
+      required this.darkMode,
+      required this.threeDOn,
+      required this.width,
+      required this.height,
+      required this.borderRadiusOfButton});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: darkMode ? Colors.grey[850] : Colors.grey[300],
+        borderRadius: BorderRadius.all(Radius.circular(borderRadiusOfButton)),
+        boxShadow: [
+          BoxShadow(
+            color: darkMode ? Colors.grey[900]! : Colors.grey[500]!,
+            offset: Offset(4.0, 4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0,
+          ),
+          BoxShadow(
+            color: darkMode ? Colors.grey[800]! : Colors.white,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0,
+          ),
+        ],
+        gradient: threeDOn
+            ? LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.grey[200]!,
+                  Colors.grey[300]!,
+                  Colors.grey[400]!,
+                  Colors.grey[500]!,
+                ],
+                stops: const [0.1, 0.3, 0.8, 0.9],
+              )
+            : null,
+      ),
+      child: Icon(
+        Icons.android,
+        size: 80,
+        color: darkMode ? Colors.white : Colors.black,
+      ),
+    );
+  }
+}
